80619764d77be9fe24aca9284dbe75d09a08667a
Thierry Fournier <thierry.fournier@ozon.io>
2021-11-28 20:35:29 +0100

    MINOR: Index pool adresses in a dichotomic index
    This avoid to store the pool id on each node, and save 8 byte for
    each node.

---
+++ radix_ptr_pool.go
@@ -0,0 +1,102 @@
+// Copyright (C) 2021 OZON SAS, Thierry Fournier <thierry.fournier@ozon.io>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, version 3
+// exclusively.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+package radix
+
+import "unsafe"
+
+type ptr_range struct {
+	start uintptr
+	end uintptr
+	pool_index int
+}
+
+/* if insert a range which overlap existing range, it panic */
+func (r *Radix)add_range(start uintptr, end uintptr, pool_index int) {
+	var left int
+	var right int
+	var pivot int
+	var insert_data []ptr_range
+
+	if start >= end {
+		return
+	}
+
+	insert_data = []ptr_range{ptr_range{
+		start: start,
+		end: end,
+		pool_index: pool_index,
+	}}
+
+	right = len(r.ptr_range)
+	for {
+		if left == right {
+			r.ptr_range = append(r.ptr_range[0:left], append(insert_data, r.ptr_range[left:]...)...)
+			return
+		}
+		pivot = (left + right) / 2
+		if end < r.ptr_range[pivot].start {
+			right = pivot
+		} else if start > r.ptr_range[pivot].end {
+			left = pivot + 1
+		} else {
+			return
+		}
+		if left > right {
+			return
+		}
+	}
+}
+
+/* Give a node and return its reference. If the node is not
+ * from a local pool return 0.
+ */
+func (r *Radix)n2r(n *Node)(uint32) {
+	var left int
+	var right int
+	var pivot int
+	var pool_index int
+	var p uintptr
+	var c *chunk
+
+	p = uintptr(unsafe.Pointer(n))
+	right = len(r.ptr_range)
+	if right == 0 {
+		panic("e")
+		return 0
+	}
+	for {
+		if left == right {
+			pool_index = r.ptr_range[left].pool_index
+			break
+		}
+		pivot = (left + right) / 2
+		if p < r.ptr_range[pivot].start {
+			right = pivot
+		} else if p > r.ptr_range[pivot].end {
+			left = pivot + 1
+		} else {
+			pool_index = r.ptr_range[pivot].pool_index
+			break
+		}
+		if left > right {
+		panic("g")
+			return 0
+		}
+	}
+	c = r.pool[pool_index]
+	return (uint32(pool_index) << 16) | (uint32(p - c.ptr) / node_sz)
+}
---
+++ radix_ptr_pool_test.go
@@ -0,0 +1,142 @@
+// Copyright (C) 2021 OZON SAS, Thierry Fournier <thierry.fournier@ozon.io>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, version 3
+// exclusively.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+package radix
+
+import "reflect"
+import "testing"
+
+func Test_ptr_range(t *testing.T) {
+	var r *Radix
+	var want []ptr_range
+
+	r = &Radix{}
+
+	r.add_range(12, 13, 1)
+	want = []ptr_range{
+		ptr_range{12, 13, 1},
+	}
+	if !reflect.DeepEqual(r.ptr_range, want) {
+		t.Errorf("Unmatched:\n   got: %#v\nexpect: %#v\n", r.ptr_range, want)
+	}
+
+	r.add_range(14, 15, 2)
+	want = []ptr_range{
+		ptr_range{12, 13, 1},
+		ptr_range{14, 15, 2},
+	}
+	if !reflect.DeepEqual(r.ptr_range, want) {
+		t.Errorf("Unmatched:\n   got: %#v\nexpect: %#v\n", r.ptr_range, want)
+	}
+
+	r.add_range(1, 2, 3)
+	want = []ptr_range{
+		ptr_range{1, 2, 3},
+		ptr_range{12, 13, 1},
+		ptr_range{14, 15, 2},
+	}
+	if !reflect.DeepEqual(r.ptr_range, want) {
+		t.Errorf("Unmatched:\n   got: %#v\nexpect: %#v\n", r.ptr_range, want)
+	}
+
+	r.add_range(6, 9, 4)
+	want = []ptr_range{
+		ptr_range{1, 2, 3},
+		ptr_range{6, 9, 4},
+		ptr_range{12, 13, 1},
+		ptr_range{14, 15, 2},
+	}
+	if !reflect.DeepEqual(r.ptr_range, want) {
+		t.Errorf("Unmatched:\n   got: %#v\nexpect: %#v\n", r.ptr_range, want)
+	}
+
+	r.add_range(7, 8, 5)
+	want = []ptr_range{
+		ptr_range{1, 2, 3},
+		ptr_range{6, 9, 4},
+		ptr_range{12, 13, 1},
+		ptr_range{14, 15, 2},
+	}
+	if !reflect.DeepEqual(r.ptr_range, want) {
+		t.Errorf("Unmatched:\n   got: %#v\nexpect: %#v\n", r.ptr_range, want)
+	}
+
+	r.add_range(6, 7, 5)
+	want = []ptr_range{
+		ptr_range{1, 2, 3},
+		ptr_range{6, 9, 4},
+		ptr_range{12, 13, 1},
+		ptr_range{14, 15, 2},
+	}
+	if !reflect.DeepEqual(r.ptr_range, want) {
+		t.Errorf("Unmatched:\n   got: %#v\nexpect: %#v\n", r.ptr_range, want)
+	}
+
+	r.add_range(7, 8, 5)
+	want = []ptr_range{
+		ptr_range{1, 2, 3},
+		ptr_range{6, 9, 4},
+		ptr_range{12, 13, 1},
+		ptr_range{14, 15, 2},
+	}
+	if !reflect.DeepEqual(r.ptr_range, want) {
+		t.Errorf("Unmatched:\n   got: %#v\nexpect: %#v\n", r.ptr_range, want)
+	}
+
+	r.add_range(6, 9, 5)
+	want = []ptr_range{
+		ptr_range{1, 2, 3},
+		ptr_range{6, 9, 4},
+		ptr_range{12, 13, 1},
+		ptr_range{14, 15, 2},
+	}
+	if !reflect.DeepEqual(r.ptr_range, want) {
+		t.Errorf("Unmatched:\n   got: %#v\nexpect: %#v\n", r.ptr_range, want)
+	}
+
+	r.add_range(5, 8, 5)
+	want = []ptr_range{
+		ptr_range{1, 2, 3},
+		ptr_range{6, 9, 4},
+		ptr_range{12, 13, 1},
+		ptr_range{14, 15, 2},
+	}
+	if !reflect.DeepEqual(r.ptr_range, want) {
+		t.Errorf("Unmatched:\n   got: %#v\nexpect: %#v\n", r.ptr_range, want)
+	}
+
+	r.add_range(8, 10, 5)
+	want = []ptr_range{
+		ptr_range{1, 2, 3},
+		ptr_range{6, 9, 4},
+		ptr_range{12, 13, 1},
+		ptr_range{14, 15, 2},
+	}
+	if !reflect.DeepEqual(r.ptr_range, want) {
+		t.Errorf("Unmatched:\n   got: %#v\nexpect: %#v\n", r.ptr_range, want)
+	}
+
+	r.add_range(5, 10, 5)
+	want = []ptr_range{
+		ptr_range{1, 2, 3},
+		ptr_range{6, 9, 4},
+		ptr_range{12, 13, 1},
+		ptr_range{14, 15, 2},
+	}
+	if !reflect.DeepEqual(r.ptr_range, want) {
+		t.Errorf("Unmatched:\n   got: %#v\nexpect: %#v\n", r.ptr_range, want)
+	}
+}
