c1890dca452f313d860e614d67daceda13584fa0
Thierry Fournier <thierry.fournier@ozon.io>
2021-11-29 23:06:26 +0100

    MINOR: Add a little bit of documentation

--- radix_string.go
+++ radix_string.go
@@ -20,6 +20,8 @@ func string_to_key(str string)([]byte, int16) {
 	return []byte(str), int16(len(str)) * 8
 }
 
+// StringLookupLonguest get a string as prefix and return the leaf which match the
+// longest part of the prefix. Return nil if none match.
 func (r *Radix)StringLookupLonguest(str string)(*Node) {
 	var length int16
 	var key []byte
@@ -34,6 +36,8 @@ func (r *Radix)StringLookupLonguest(str string)(*Node) {
 	return r.LookupLonguest(&key, length)
 }
 
+// StringLookupLonguestPath take the radix tree and a string as prefix, return the list
+// of all leaf matching the prefix. If none match, return nil
 func (r *Radix)StringLookupLonguestPath(str string)([]*Node) {
 	var length int16
 	var key []byte
@@ -48,6 +52,8 @@ func (r *Radix)StringLookupLonguestPath(str string)([]*Node) {
 	return r.LookupLonguestPath(&key, length)
 }
 
+// Get gets a string as prefix and return exact match of the prefix. Exact match
+// is a node wich match the prefix bit and the length.
 func (r *Radix)StringGet(str string)(*Node) {
 	var length int16
 	var key []byte
@@ -62,6 +68,9 @@ func (r *Radix)StringGet(str string)(*Node) {
 	return r.Get(&key, length)
 }
 
+// StringInsert string as prefix in the tree. The tree accept only unique value, if
+// the prefix already exists in the tree, return existing leaf,
+// otherwaise return nil.
 func (r *Radix)StringInsert(str string, data interface{})(*Node, bool) {
 	var length int16
 	var key []byte
@@ -76,6 +85,8 @@ func (r *Radix)StringInsert(str string, data interface{})(*Node, bool) {
 	return r.Insert(&key, length, data)
 }
 
+// StringDelete lookup string and remove it. does nothing
+// if the string not exists.
 func (r *Radix)StringDelete(str string)() {
 	var node *Node
 	var length int16
@@ -97,6 +108,8 @@ func (r *Radix)StringDelete(str string)() {
 	r.Delete(node)
 }
 
+// StringNewIter return struct Iter for browsing all nodes there children
+// match the string prefix.
 func (r *Radix)StringNewIter(str string)(*Iter) {
 	var length int16
 	var key []byte
@@ -105,6 +118,7 @@ func (r *Radix)StringNewIter(str string)(*Iter) {
 	return r.NewIter(&key, length)
 }
 
+// StringGetKey convert node key/length prefix to string
 func (n *Node)StringGetKey()(string) {
 	return string(n.node.Bytes)
 }
